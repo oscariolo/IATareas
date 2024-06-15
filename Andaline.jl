6f3e7c80bb9ce299c344bda62aa13b169fd7cad1
oscariolo <oscariolopinop21@gmail.com>
2024-06-15 13:03:04 -0500

    Add files via upload

---
+++ Andaline.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.41
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 29681a92-23a8-11ef-268c-21ac73308d43
+
+
+# ╔═╡ Cell order:
+# ╠═29681a92-23a8-11ef-268c-21ac73308d43
